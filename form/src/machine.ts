evgkch/fsmjs-examples

The examples run on the inspector's widgets from the registry
- the hand-drawn strips give way to the inspector's widgets: the readout
  stands on the machine's bar, the floor stands in two columns
- a third example: a form over a flaky wire — the send carries its attempt
  number, a foreign answer matches no rule and is dropped, three refusals
  spend the budget; the review signs with real ECDSA
- the review's gate files no finding for a dead-end state: a final state
  is usually the intended one
- the pages are pressed by a test, and the inspector arrives from the
  registry (0.0.6)
- the walkthroughs name facts, not figures, in both languages; the index
  lists the form

form/src/machine.ts
@@ -1,0 +1,156 @@
+/**
+ * A form as a machine, sent over a flaky wire. Typing folds into the context and validation
+ * is a pure reading of it. `submit` is a guard cascade: whole, the form goes to `sending`;
+ * not whole, the same event stays in `editing` and marks every field. The send is a phase:
+ * where there is no `input` rule the form is locked without a single flag.
+ *
+ * Every send carries the attempt's number — its ticket — and the answer brings the ticket
+ * back. `fail` is a second cascade: the last allowed attempt gives the form up, an earlier
+ * one is a refusal with a retry, and an answer whose ticket is not the one in flight matches
+ * no rule at all — a duplicate or a straggler is dropped, and `dispatch` says so with `false`.
+ *
+ *   editing ── submit·whole ─▸ sending ── ok·mine ──▸ sent
+ *   sending ── fail·mine ───▸ refused ── retry ────▸ sending (ticket advanced)
+ *   sending ── fail·spent ──▸ failed
+ *   refused, failed ── input ─▸ editing (the budget starts over)
+ */
+import { StateMachine } from "@evgkch/fsmjs";
+import type {
+  Failed,
+  Fault,
+  Field,
+  Fields,
+  Filling,
+  InFlight,
+  Q,
+  Refused,
+  Touched,
+  Λ,
+  Σ,
+} from "./types.js";
+
+/** Attempts the budget allows before the machine gives the form up. */
+export const TRIES = 3;
+
+/** What is wrong with the fields, read fresh on every keystroke. */
+export function faultsOf(f: Fields): Fault[] {
+  return [
+    ...(f.name.trim() === ""
+      ? [{ field: "name", say: "a name is required" } as const]
+      : []),
+    ...(/^\S+@\S+\.\S+$/.test(f.email)
+      ? []
+      : [{ field: "email", say: "not an address" } as const]),
+    ...(/^\d+$/.test(f.amount) && +f.amount >= 1 && +f.amount <= 1000
+      ? []
+      : [{ field: "amount", say: "a number, 1 to 1000" } as const]),
+  ];
+}
+
+/** One keystroke folded in: the field replaced, the faults re-read, the touches kept. */
+function put(c: Filling, p: { field: Field; value: string }): Filling {
+  const fields = { ...c.fields, [p.field]: p.value };
+  return { fields, faults: faultsOf(fields), touched: c.touched };
+}
+
+/** The reader left the field: from now on its fault is said out loud. */
+function mark(c: Filling, p: { field: Field }): Filling {
+  return { ...c, touched: { ...c.touched, [p.field]: true } };
+}
+
+/** A submit on a faulty form says every fault out loud. */
+function markAll(c: Filling): Filling {
+  return { ...c, touched: { name: true, email: true, amount: true } };
+}
+
+/** Nothing is wrong: the one condition `submit` fires under. */
+export function whole(c: Filling): boolean {
+  return c.faults.length === 0;
+}
+
+/** The answer is to the attempt in flight — any other is dropped. */
+function mine(c: InFlight, p: { attempt: number }): boolean {
+  return p.attempt === c.attempt;
+}
+
+/** The answer is to the attempt in flight, and the budget allows no further one. */
+function spent(c: InFlight, p: { attempt: number }): boolean {
+  return p.attempt === c.attempt && c.attempt >= TRIES;
+}
+
+/** The first attempt: the ticket starts at 1. */
+function first(c: Filling): InFlight {
+  return { ...c, attempt: 1 };
+}
+
+/** The next attempt: the same form, the ticket advanced. */
+function again(c: Refused): InFlight {
+  return {
+    fields: c.fields,
+    faults: c.faults,
+    touched: c.touched,
+    attempt: c.attempt + 1,
+  };
+}
+
+/** The refusal, remembered beside the attempt. */
+function remember(c: InFlight, p: { attempt: number; why: string }): Refused {
+  return { ...c, why: p.why };
+}
+
+/** The budget is spent: the count is dropped, the last reason stays. */
+function giveUp(c: InFlight, p: { attempt: number; why: string }): Failed {
+  return { fields: c.fields, faults: c.faults, touched: c.touched, why: p.why };
+}
+
+/** An accepted order: the fields and the server's receipt; nothing of the editing. */
+function accepted(
+  c: InFlight,
+  p: { attempt: number; receipt: string },
+): { fields: Fields; receipt: string } {
+  return { fields: c.fields, receipt: p.receipt };
+}
+
+/** What `send` carries: the ticket and the fields. Reads the context after the move. */
+function ticketed(c: InFlight): { attempt: number; fields: Fields } {
+  return { attempt: c.attempt, fields: c.fields };
+}
+
+const EMPTY: Fields = { name: "", email: "", amount: "" };
+const UNTOUCHED: Touched = { name: false, email: false, amount: false };
+
+export const form = new StateMachine<Q, Σ, Λ>(
+  {
+    editing: {
+      input: [{ to: ["editing", put] }],
+      leave: [{ to: ["editing", mark] }],
+      submit: [
+        { when: whole, to: ["sending", first], emit: ["send", ticketed] },
+        { to: ["editing", markAll] },
+      ],
+    },
+    sending: {
+      ok: [{ when: mine, to: ["sent", accepted] }],
+      // The order matters: `spent` is the narrower case and stands first. An answer that
+      // passes neither guard matches no rule — that is the drop.
+      fail: [
+        { when: spent, to: ["failed", giveUp] },
+        { when: mine, to: ["refused", remember] },
+      ],
+    },
+    refused: {
+      // Touching any field is editing again; the refusal's reason stays behind.
+      input: [{ to: ["editing", put] }],
+      retry: [{ to: ["sending", again], emit: ["send", ticketed] }],
+    },
+    failed: {
+      // Editing resets the budget: the next submit is a first attempt again.
+      input: [{ to: ["editing", put] }],
+    },
+    sent: {},
+  },
+  {
+    type: "editing",
+    context: { fields: EMPTY, faults: faultsOf(EMPTY), touched: UNTOUCHED },
+  },
+);

form/src/types.ts
@@ -1,0 +1,55 @@
+/**
+ * The vocabulary of the form: three fields, the faults a reading of them yields, and the
+ * carriers Q, Σ, Λ. The context belongs to the phase: an attempt on the wire carries its
+ * number, a refusal carries the server's answer beside it, `sent` carries the receipt.
+ */
+import type { IEvent, IState, Merge } from "@evgkch/fsmjs";
+
+export type Fields = {
+  name: string;
+  email: string;
+  amount: string;
+};
+
+export type Field = keyof Fields;
+
+/** One thing wrong with one field, said for the reader. */
+export type Fault = { field: Field; say: string };
+
+/** Which fields the reader has left at least once — a fault is shown only for those. */
+export type Touched = Readonly<Record<Field, boolean>>;
+
+/** The form as it is being filled: the fields, what is wrong right now, and what to say it for. */
+export type Filling = {
+  fields: Fields;
+  faults: readonly Fault[];
+  touched: Touched;
+};
+
+/** An attempt on the wire or refused: the same form and the attempt's number — its ticket. */
+export type InFlight = Filling & { attempt: number };
+
+/** The server refused this attempt: the reason beside the count. */
+export type Refused = InFlight & { why: string };
+
+/** The budget is spent: the form and the last reason; nothing left to count. */
+export type Failed = Filling & { why: string };
+
+export type Q = Merge<
+  | IState<"editing", Filling>
+  | IState<"sending", InFlight>
+  | IState<"refused", Refused>
+  | IState<"failed", Failed>
+  | IState<"sent", { fields: Fields; receipt: string }>
+>;
+
+export type Σ = Merge<
+  | IEvent<"input", { field: Field; value: string }>
+  | IEvent<"leave", { field: Field }>
+  | IEvent<"submit">
+  | IEvent<"ok", { attempt: number; receipt: string }>
+  | IEvent<"fail", { attempt: number; why: string }>
+  | IEvent<"retry">
+>;
+
+export type Λ = IEvent<"send", { attempt: number; fields: Fields }>;

selection-rect/src/main.ts
@@ -8,7 +8,8 @@
  * is held by the schema, not by a chain of `if`s in the view.
  */
 import type { FsmState } from "@evgkch/fsmjs";
-import { history, log, rules } from "@evgkch/fsmjs/debug";
+import { history, log } from "@evgkch/fsmjs/debug";
+import { FsmjsDesk, fromMachine } from "@evgkch/fsmjs-inspector/ui";
 import { handleAt, norm } from "./geometry.js";
 import { inside, sel } from "./machine.js";
 import type { Point, Rect, Sel, Spot } from "./types.js";
@@ -17,7 +18,6 @@ const area = document.getElementById("area")!;
 const box = document.getElementById("box")!;
 const rectOut = document.getElementById("rect")!;
 const undoOut = document.getElementById("undo")!;
-const logOut = document.getElementById("log")!;
 
 /**
  * Pointer position relative to the drawing area, in whole pixels, with the area's own size
@@ -92,19 +92,12 @@ area.addEventListener("pointermove", (e) => {
 // `history` will not do: it records every `move`, so undo would crawl back a
 // drag one pointer sample at a time. A condition inside `log`'s sink looks at
 // the (source, target) pair of one transition and keeps only the step *into* a
-// drag — one entry per operation, holding where the machine stood before it.
+// drag — one entry per operation.
 
 const DRAG = ["drawing", "moving", "resizing"];
 
-/**
- * One undo entry: where the machine stood before the drag, and the log as it read then.
- *
- * The position is `t.source` itself — both halves of S = Q × V, a `State`, taken
- * straight off the transition. A bare rectangle was not enough: the first drag begins in
- * `empty`, and going back to it as `ready` left the page showing a 0×0 selection the
- * machine did not believe in.
- */
-type Undone = { at: number; log: Line[] };
+/** One undo entry: where in the record the machine stood before the drag. */
+type Undone = { at: number };
 const undo: Undone[] = [];
 
 /**
@@ -120,17 +113,13 @@ const undo: Undone[] = [];
  */
 const past = history(sel);
 
-log(
-  sel,
-  rules((line, t) => {
-    // `history` subscribed first, so its index already points at the state this transition
-    // reached; the state the drag began at is the one before it.
-    if (DRAG.includes(t.target.type) && !DRAG.includes(t.source.type))
-      undo.push({ at: past.index - 1, log: entries.map((e) => ({ ...e })) });
-    render(t.target);
-    trace(line);
-  }),
-);
+log(sel, (t) => {
+  // `history` subscribed first, so its index already points at the state this transition
+  // reached; the state the drag began at is the one before it.
+  if (DRAG.includes(t.target.type) && !DRAG.includes(t.source.type))
+    undo.push({ at: past.index - 1 });
+  render(t.target);
+});
 
 /**
  * Wherever the machine is put back, the page draws where it now is.
@@ -149,13 +138,10 @@ past.rx.on("moved", () => {
   render(at);
 });
 
-/** Back one whole drag: the log strip as it read then, and the record put back to where it began. */
+/** Back one whole drag: the record put back to where it began. */
 function undoDrag() {
   const back = undo.pop();
   if (!back) return;
-
-  entries.splice(0, entries.length, ...back.log);
-  paintLog();
   past.jump(back.at);
 }
 
@@ -189,33 +175,13 @@ function render(at: FsmState<Sel>) {
   undoOut.textContent = String(undo.length);
 }
 
-/**
- * The transition log, newest first, with runs folded.
- *
- * A drag is one transition repeated: `move` fires per pointer sample, so
- * `drawing → drawing` would push the interesting lines off the strip within a
- * second. Identical consecutive lines therefore collapse into one and carry a
- * repeat count instead.
- */
-type Line = { line: string; count: number };
-
-const LINES = 12;
-const entries: Line[] = [];
-
-function trace(line: string) {
-  const head = entries[0];
-  if (head?.line === line) head.count++;
-  else entries.unshift({ line, count: 1 });
-
-  entries.length = Math.min(entries.length, LINES);
-  paintLog();
-}
-
-/** Kept apart from `trace` because undo rewrites the whole strip at once. */
-function paintLog() {
-  logOut.textContent = entries
-    .map((e) => (e.count > 1 ? `${e.line} (×${e.count})` : e.line))
-    .join("\n");
-}
-
-render(sel.state);
+// ── the machine, drawn ────────────────────────────────────────────────────────
+// The inspector's widgets on the same `sel`: the desk wires each to one subject and focus and
+// gives it a switch; the widgets hear the machine themselves.
+const desk = new FsmjsDesk();
+desk.wiring = { subject: fromMachine(sel, { history: past }) };
+document.getElementById("board")!.append(desk);
+for (const widget of document.querySelectorAll<HTMLElement>(
+  "fsmjs-legend, fsmjs-diagram, fsmjs-history",
+))
+  desk.enroll(widget as Parameters<typeof desk.enroll>[0]);

evgkch/machjs

form/src/machine.ts
@@ -1,0 +1,156 @@
+/**
+ * A form as a machine, sent over a flaky wire. Typing folds into the context and validation
+ * is a pure reading of it. `submit` is a guard cascade: whole, the form goes to `sending`;
+ * not whole, the same event stays in `editing` and marks every field. The send is a phase:
+ * where there is no `input` rule the form is locked without a single flag.
+ *
+ * Every send carries the attempt's number — its ticket — and the answer brings the ticket
+ * back. `fail` is a second cascade: the last allowed attempt gives the form up, an earlier
+ * one is a refusal with a retry, and an answer whose ticket is not the one in flight matches
+ * no rule at all — a duplicate or a straggler is dropped, and `dispatch` says so with `false`.
+ *
+ *   editing ── submit·whole ─▸ sending ── ok·mine ──▸ sent
+ *   sending ── fail·mine ───▸ refused ── retry ────▸ sending (ticket advanced)
+ *   sending ── fail·spent ──▸ failed
+ *   refused, failed ── input ─▸ editing (the budget starts over)
+ */
+import { StateMachine } from "@evgkch/machjs";
+import type {
+  Failed,
+  Fault,
+  Field,
+  Fields,
+  Filling,
+  InFlight,
+  Q,
+  Refused,
+  Touched,
+  Λ,
+  Σ,
+} from "./types.js";
+
+/** Attempts the budget allows before the machine gives the form up. */
+export const TRIES = 3;
+
+/** What is wrong with the fields, read fresh on every keystroke. */
+export function faultsOf(f: Fields): Fault[] {
+  return [
+    ...(f.name.trim() === ""
+      ? [{ field: "name", say: "a name is required" } as const]
+      : []),
+    ...(/^\S+@\S+\.\S+$/.test(f.email)
+      ? []
+      : [{ field: "email", say: "not an address" } as const]),
+    ...(/^\d+$/.test(f.amount) && +f.amount >= 1 && +f.amount <= 1000
+      ? []
+      : [{ field: "amount", say: "a number, 1 to 1000" } as const]),
+  ];
+}
+
+/** One keystroke folded in: the field replaced, the faults re-read, the touches kept. */
+function put(c: Filling, p: { field: Field; value: string }): Filling {
+  const fields = { ...c.fields, [p.field]: p.value };
+  return { fields, faults: faultsOf(fields), touched: c.touched };
+}
+
+/** The reader left the field: from now on its fault is said out loud. */
+function mark(c: Filling, p: { field: Field }): Filling {
+  return { ...c, touched: { ...c.touched, [p.field]: true } };
+}
+
+/** A submit on a faulty form says every fault out loud. */
+function markAll(c: Filling): Filling {
+  return { ...c, touched: { name: true, email: true, amount: true } };
+}
+
+/** Nothing is wrong: the one condition `submit` fires under. */
+export function whole(c: Filling): boolean {
+  return c.faults.length === 0;
+}
+
+/** The answer is to the attempt in flight — any other is dropped. */
+function mine(c: InFlight, p: { attempt: number }): boolean {
+  return p.attempt === c.attempt;
+}
+
+/** The answer is to the attempt in flight, and the budget allows no further one. */
+function spent(c: InFlight, p: { attempt: number }): boolean {
+  return p.attempt === c.attempt && c.attempt >= TRIES;
+}
+
+/** The first attempt: the ticket starts at 1. */
+function first(c: Filling): InFlight {
+  return { ...c, attempt: 1 };
+}
+
+/** The next attempt: the same form, the ticket advanced. */
+function again(c: Refused): InFlight {
+  return {
+    fields: c.fields,
+    faults: c.faults,
+    touched: c.touched,
+    attempt: c.attempt + 1,
+  };
+}
+
+/** The refusal, remembered beside the attempt. */
+function remember(c: InFlight, p: { attempt: number; why: string }): Refused {
+  return { ...c, why: p.why };
+}
+
+/** The budget is spent: the count is dropped, the last reason stays. */
+function giveUp(c: InFlight, p: { attempt: number; why: string }): Failed {
+  return { fields: c.fields, faults: c.faults, touched: c.touched, why: p.why };
+}
+
+/** An accepted order: the fields and the server's receipt; nothing of the editing. */
+function accepted(
+  c: InFlight,
+  p: { attempt: number; receipt: string },
+): { fields: Fields; receipt: string } {
+  return { fields: c.fields, receipt: p.receipt };
+}
+
+/** What `send` carries: the ticket and the fields. Reads the context after the move. */
+function ticketed(c: InFlight): { attempt: number; fields: Fields } {
+  return { attempt: c.attempt, fields: c.fields };
+}
+
+const EMPTY: Fields = { name: "", email: "", amount: "" };
+const UNTOUCHED: Touched = { name: false, email: false, amount: false };
+
+export const form = new StateMachine<Q, Σ, Λ>(
+  {
+    editing: {
+      input: [{ to: ["editing", put] }],
+      leave: [{ to: ["editing", mark] }],
+      submit: [
+        { when: whole, to: ["sending", first], emit: ["send", ticketed] },
+        { to: ["editing", markAll] },
+      ],
+    },
+    sending: {
+      ok: [{ when: mine, to: ["sent", accepted] }],
+      // The order matters: `spent` is the narrower case and stands first. An answer that
+      // passes neither guard matches no rule — that is the drop.
+      fail: [
+        { when: spent, to: ["failed", giveUp] },
+        { when: mine, to: ["refused", remember] },
+      ],
+    },
+    refused: {
+      // Touching any field is editing again; the refusal's reason stays behind.
+      input: [{ to: ["editing", put] }],
+      retry: [{ to: ["sending", again], emit: ["send", ticketed] }],
+    },
+    failed: {
+      // Editing resets the budget: the next submit is a first attempt again.
+      input: [{ to: ["editing", put] }],
+    },
+    sent: {},
+  },
+  {
+    type: "editing",
+    context: { fields: EMPTY, faults: faultsOf(EMPTY), touched: UNTOUCHED },
+  },
+);

form/src/types.ts
@@ -1,0 +1,55 @@
+/**
+ * The vocabulary of the form: three fields, the faults a reading of them yields, and the
+ * carriers Q, Σ, Λ. The context belongs to the phase: an attempt on the wire carries its
+ * number, a refusal carries the server's answer beside it, `sent` carries the receipt.
+ */
+import type { IEvent, IState, Merge } from "@evgkch/machjs";
+
+export type Fields = {
+  name: string;
+  email: string;
+  amount: string;
+};
+
+export type Field = keyof Fields;
+
+/** One thing wrong with one field, said for the reader. */
+export type Fault = { field: Field; say: string };
+
+/** Which fields the reader has left at least once — a fault is shown only for those. */
+export type Touched = Readonly<Record<Field, boolean>>;
+
+/** The form as it is being filled: the fields, what is wrong right now, and what to say it for. */
+export type Filling = {
+  fields: Fields;
+  faults: readonly Fault[];
+  touched: Touched;
+};
+
+/** An attempt on the wire or refused: the same form and the attempt's number — its ticket. */
+export type InFlight = Filling & { attempt: number };
+
+/** The server refused this attempt: the reason beside the count. */
+export type Refused = InFlight & { why: string };
+
+/** The budget is spent: the form and the last reason; nothing left to count. */
+export type Failed = Filling & { why: string };
+
+export type Q = Merge<
+  | IState<"editing", Filling>
+  | IState<"sending", InFlight>
+  | IState<"refused", Refused>
+  | IState<"failed", Failed>
+  | IState<"sent", { fields: Fields; receipt: string }>
+>;
+
+export type Σ = Merge<
+  | IEvent<"input", { field: Field; value: string }>
+  | IEvent<"leave", { field: Field }>
+  | IEvent<"submit">
+  | IEvent<"ok", { attempt: number; receipt: string }>
+  | IEvent<"fail", { attempt: number; why: string }>
+  | IEvent<"retry">
+>;
+
+export type Λ = IEvent<"send", { attempt: number; fields: Fields }>;

selection-rect/src/main.ts
@@ -7,8 +7,9 @@
  * machine's partiality doing real work: the set of events acceptable right now
  * is held by the schema, not by a chain of `if`s in the view.
  */
-import type { FsmState } from "@evgkch/fsmjs";
-import { history, log, rules } from "@evgkch/fsmjs/debug";
+import type { FsmState } from "@evgkch/machjs";
+import { history, log } from "@evgkch/machjs/debug";
+import { MachjsDesk, fromMachine } from "@evgkch/machjs-inspector/ui";
 import { handleAt, norm } from "./geometry.js";
 import { inside, sel } from "./machine.js";
 import type { Point, Rect, Sel, Spot } from "./types.js";
@@ -17,7 +18,6 @@ const area = document.getElementById("area")!;
 const box = document.getElementById("box")!;
 const rectOut = document.getElementById("rect")!;
 const undoOut = document.getElementById("undo")!;
-const logOut = document.getElementById("log")!;
 
 /**
  * Pointer position relative to the drawing area, in whole pixels, with the area's own size
@@ -92,19 +92,12 @@ area.addEventListener("pointermove", (e) => {
 // `history` will not do: it records every `move`, so undo would crawl back a
 // drag one pointer sample at a time. A condition inside `log`'s sink looks at
 // the (source, target) pair of one transition and keeps only the step *into* a
-// drag — one entry per operation, holding where the machine stood before it.
+// drag — one entry per operation.
 
 const DRAG = ["drawing", "moving", "resizing"];
 
-/**
- * One undo entry: where the machine stood before the drag, and the log as it read then.
- *
- * The position is `t.source` itself — both halves of S = Q × V, a `State`, taken
- * straight off the transition. A bare rectangle was not enough: the first drag begins in
- * `empty`, and going back to it as `ready` left the page showing a 0×0 selection the
- * machine did not believe in.
- */
-type Undone = { at: number; log: Line[] };
+/** One undo entry: where in the record the machine stood before the drag. */
+type Undone = { at: number };
 const undo: Undone[] = [];
 
 /**
@@ -120,17 +113,13 @@ const undo: Undone[] = [];
  */
 const past = history(sel);
 
-log(
-  sel,
-  rules((line, t) => {
-    // `history` subscribed first, so its index already points at the state this transition
-    // reached; the state the drag began at is the one before it.
-    if (DRAG.includes(t.target.type) && !DRAG.includes(t.source.type))
-      undo.push({ at: past.index - 1, log: entries.map((e) => ({ ...e })) });
-    render(t.target);
-    trace(line);
-  }),
-);
+log(sel, (t) => {
+  // `history` subscribed first, so its index already points at the state this transition
+  // reached; the state the drag began at is the one before it.
+  if (DRAG.includes(t.target.type) && !DRAG.includes(t.source.type))
+    undo.push({ at: past.index - 1 });
+  render(t.target);
+});
 
 /**
  * Wherever the machine is put back, the page draws where it now is.
@@ -149,13 +138,10 @@ past.rx.on("moved", () => {
   render(at);
 });
 
-/** Back one whole drag: the log strip as it read then, and the record put back to where it began. */
+/** Back one whole drag: the record put back to where it began. */
 function undoDrag() {
   const back = undo.pop();
   if (!back) return;
-
-  entries.splice(0, entries.length, ...back.log);
-  paintLog();
   past.jump(back.at);
 }
 
@@ -189,33 +175,13 @@ function render(at: FsmState<Sel>) {
   undoOut.textContent = String(undo.length);
 }
 
-/**
- * The transition log, newest first, with runs folded.
- *
- * A drag is one transition repeated: `move` fires per pointer sample, so
- * `drawing → drawing` would push the interesting lines off the strip within a
- * second. Identical consecutive lines therefore collapse into one and carry a
- * repeat count instead.
- */
-type Line = { line: string; count: number };
-
-const LINES = 12;
-const entries: Line[] = [];
-
-function trace(line: string) {
-  const head = entries[0];
-  if (head?.line === line) head.count++;
-  else entries.unshift({ line, count: 1 });
-
-  entries.length = Math.min(entries.length, LINES);
-  paintLog();
-}
-
-/** Kept apart from `trace` because undo rewrites the whole strip at once. */
-function paintLog() {
-  logOut.textContent = entries
-    .map((e) => (e.count > 1 ? `${e.line} (×${e.count})` : e.line))
-    .join("\n");
-}
-
-render(sel.state);
+// ── the machine, drawn ────────────────────────────────────────────────────────
+// The inspector's widgets on the same `sel`: the desk wires each to one subject and focus and
+// gives it a switch; the widgets hear the machine themselves.
+const desk = new MachjsDesk();
+desk.wiring = { subject: fromMachine(sel, { history: past }) };
+document.getElementById("board")!.append(desk);
+for (const widget of document.querySelectorAll<HTMLElement>(
+  "machjs-legend, machjs-diagram, machjs-history",
+))
+  desk.enroll(widget as Parameters<typeof desk.enroll>[0]);